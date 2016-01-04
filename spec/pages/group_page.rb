d4e49dc80828e367063e03e758dad48d847060fa
lockys <locky4567@gmail.com>
2016-01-04 22:45:02 +0800

    adjust spec

--- spec/pages/group_page.rb
+++ spec/pages/group_page.rb
@@ -2,7 +2,7 @@ require 'page-object'
 
 class GroupPage
   include PageObject
-  page_url "http://localhost:9292/group"
+  page_url "http://localhost:3000/group"
 
   ##TODO: create element mapping
 end
