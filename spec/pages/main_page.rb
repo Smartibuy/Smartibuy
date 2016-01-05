7a1ef1668293679291aed0ca35c71e92dbcb7d92
lockys <locky4567@gmail.com>
2016-01-05 10:42:32 +0800

    change server port

--- spec/pages/main_page.rb
+++ spec/pages/main_page.rb
@@ -2,7 +2,7 @@ require 'page-object'
 
 class MainPage
   include PageObject
-  page_url "http://localhost:3000/"
+  page_url "http://localhost:9292/"
 
   link(:about_link, text: '關於 Smartibuy!')
   link(:group_link, text: '賣場列表')
